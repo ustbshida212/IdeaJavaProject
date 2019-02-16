46e5e1e72b6099d2b8c6533513ded8deb76272c5
jinhui_shi <395649086@qq.com>
2019-02-16 18:12:30 +0800

    修改代码

--- FirstGitTest/src/com/gittest/GitTest.java
+++ FirstGitTest/src/com/gittest/GitTest.java
@@ -4,4 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git");
     }
+    public void test(){
+        System.out.println("new code ");
+    }
 }
